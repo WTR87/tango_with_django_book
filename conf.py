52c8d5006f70db5a2ee3ce3ca17e24d11a35e9a1
WTR87 <hello2125@gmail.com>
2015-09-24 15:17:46 +0200

    Update conf.py

--- conf.py
+++ conf.py
@@ -185,18 +185,19 @@ htmlhelp_basename = 'How_To_Tango_With_Djangodoc'
 
 # -- Options for LaTeX output --------------------------------------------------
 
+latex_elements = {
 # The paper size ('letterpaper' or 'a4paper').
-papersize = 'a4paper'
+'papersize': 'a4paper',
 
 # The font size ('10pt', '11pt' or '12pt').
-pointsize = '11pt'
+'pointsize': '11pt',
 
 # Additional stuff for the LaTeX preamble.
-preamble' = r'''
+'preamble': r'''
     \usepackage{charter}
     \usepackage[defaultsans]{lato}
     \usepackage{inconsolata}
-'''
+''',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
